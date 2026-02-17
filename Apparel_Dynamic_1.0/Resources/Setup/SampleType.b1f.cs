cfaf2d7bbd4afb324ed3fdcfe9ca2707ed74f779
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-02-17 12:40:17 +0600

    Menu ready for all forms

---
+++ Apparel_Dynamic_1.0/Resources/Setup/SampleType.b1f.cs
@@ -0,0 +1,30 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apparel_Dynamic_1._0.Resources.Setup
+{
+    [FormAttribute("Apparel_Dynamic_1._0.Resources.Setup.SampleType", "Resources/Setup/SampleType.b1f")]
+    class SampleType : UserFormBase
+    {
+        public SampleType()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}
