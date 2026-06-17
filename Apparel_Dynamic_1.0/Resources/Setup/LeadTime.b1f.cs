f9963de1f97af3e06663295b5352b7144a196996
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-06-17 15:50:34 +0600

    feat: adding new form called LeadTime

---
+++ Apparel_Dynamic_1.0/Resources/Setup/LeadTime.b1f.cs
@@ -0,0 +1,30 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apparel_Dynamic_1._0.Resources.Setup
+{
+    [FormAttribute("Apparel_Dynamic_1._0.Resources.Setup.LeadTime", "Resources/Setup/LeadTime.b1f")]
+    class LeadTime : UserFormBase
+    {
+        public LeadTime()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}
